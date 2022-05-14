5c11c3488c5f3e5b3d9052f97ab9aa25c0fb077a
ryousuke.oohashi <r.o1002eight@gmail.com>
2022-05-14 22:03:45 +0900

    EmployeeValidator,EmployeeService作成

--- src/main/java/actions/views/EmployeeConverter.java
+++ src/main/java/actions/views/EmployeeConverter.java
@@ -8,7 +8,7 @@ import constants.JpaConst;
 import models.Employee;
 
 /**
- * 従業員データのDTOモデル⇔Viewモデルの変換を行う
+ * 従業員データのDTOモデル⇔Viewモデルの変換を行うクラス
  *
  */
 public class EmployeeConverter {
@@ -16,28 +16,29 @@ public class EmployeeConverter {
     /**
      * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
      * @param ev EmployeeViewのインスタンス
-     * @return Employeeのクラス
+     * @return Employeeのインスタンス
      */
     public static Employee toModel(EmployeeView ev) {
 
         return new Employee(
                 ev.getId(),
-           ev.getCode(),
-           ev.getName(),
-           ev.getPassword(),
-           ev.getAdminFlag() == null
-                    ? null
-                    : ev.getAdminFlag() == AttributeConst.ROLE_ADMIN.getIntegerValue()
-                            ? JpaConst.ROLE_ADMIN
-                            : JpaConst.ROLE_GENERAL,
-           ev.getCreatedAt(),
-           ev.getUpdatedAt(),
-           ev.getDeleteFlag() == null
-                   ? null
-                   : ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()
-                           ? JpaConst.EMP_DEL_TRUE
-                           : JpaConst.EMP_DEL_FALSE);
+                ev.getCode(),
+                ev.getName(),
+                ev.getPassword(),
+                ev.getAdminFlag() == null
+                        ? null
+                        : ev.getAdminFlag() == AttributeConst.ROLE_ADMIN.getIntegerValue()
+                                ? JpaConst.ROLE_ADMIN
+                                : JpaConst.ROLE_GENERAL,
+                ev.getCreatedAt(),
+                ev.getUpdatedAt(),
+                ev.getDeleteFlag() == null
+                        ? null
+                        : ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()
+                                ? JpaConst.EMP_DEL_TRUE
+                                : JpaConst.EMP_DEL_FALSE);
     }
+
     /**
      * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
      * @param e Employeeのインスタンス
@@ -63,17 +64,17 @@ public class EmployeeConverter {
                 e.getUpdatedAt(),
                 e.getDeleteFlag() == null
                         ? null
-                        :e.getDeleteFlag() == JpaConst.EMP_DEL_TRUE
+                        : e.getDeleteFlag() == JpaConst.EMP_DEL_TRUE
                                 ? AttributeConst.DEL_FLAG_TRUE.getIntegerValue()
                                 : AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
     }
 
     /**
      * DTOモデルのリストからViewモデルのリストを作成する
-     * @param List DTOモデルのリスト
+     * @param list DTOモデルのリスト
      * @return Viewモデルのリスト
      */
-    public static List<EmployeeView> toViewList(List<Employee> list){
+    public static List<EmployeeView> toViewList(List<Employee> list) {
         List<EmployeeView> evs = new ArrayList<>();
 
         for (Employee e : list) {
@@ -86,9 +87,9 @@ public class EmployeeConverter {
     /**
      * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
      * @param e DTOモデル(コピー先)
-     * @param ev Viewモデル()コピー元
+     * @param ev Viewモデル(コピー元)
      */
-    public static void CONYYewToModel(Employee e, EmployeeView ev) {
+    public static void copyViewToModel(Employee e, EmployeeView ev) {
         e.setId(ev.getId());
         e.setCode(ev.getCode());
         e.setName(ev.getName());
@@ -97,5 +98,7 @@ public class EmployeeConverter {
         e.setCreatedAt(ev.getCreatedAt());
         e.setUpdatedAt(ev.getUpdatedAt());
         e.setDeleteFlag(ev.getDeleteFlag());
+
     }
+
 }
